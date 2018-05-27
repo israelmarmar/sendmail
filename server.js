faf9fcbcb3a53b0611dc6acbb5f31ce5c4aa19a3
Israel Marques Martins <email.israel.br@gmail.com>
2018-05-27 13:18:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -40,3 +40,22 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
  console.log("ligado");
 });
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://israelmarmar.github.io/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
