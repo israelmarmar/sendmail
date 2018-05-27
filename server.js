e84bcc7f5ae0f156d9e27cb529f9493f00df2ab4
Israel Marques Martins <email.israel.br@gmail.com>
2018-05-27 14:03:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.listen(port, function () {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://israelmarmar.github.io/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://israelmarmar.github.io');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
