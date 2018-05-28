af518ba98218faed60ad02e2c481e59dcc9abda3
Israel Marques Martins <email.israel.br@gmail.com>
2018-05-28 07:40:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+});
 
 app.get('/', function (req, res) {
 
@@ -39,23 +46,4 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
  console.log("ligado");
-});
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://israelmarmar.github.io/portifolio/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
 });
